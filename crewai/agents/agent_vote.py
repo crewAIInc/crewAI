99853e55cd341aaa93b1f69de28d8810202e388d
João Moura <joaomdmoura@mgail.com>
2023-12-27 16:17:36 -0300

    removing AgentVote class

--- crewai/agents/agent_vote.py
+++
@@ -1,5 +0,0 @@
-from pydantic.v1 import BaseModel, Field
-
-class AgentVote(BaseModel):
-    task: str = Field(description="Task to be executed by the agent")
-    agent_vote: str = Field(description="Agent that will execute the task")
