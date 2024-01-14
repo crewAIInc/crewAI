1954be442d5b1e5f4a237a0aa0cf0b9eb047df30
João Moura <joaomdmoura@gmail.com>
2024-01-13 23:25:35 -0300

    creating Logger and RPMController

---
+++ src/crewai/utilities/logger.py
@@ -0,0 +1,11 @@
+class Logger:
+    def __init__(self, verbose_level=0):
+        verbose_level = (
+            2 if isinstance(verbose_level, bool) and verbose_level else verbose_level
+        )
+        self.verbose_level = verbose_level
+
+    def log(self, level, message):
+        level_map = {"debug": 1, "info": 2}
+        if self.verbose_level and level_map.get(level, 0) <= self.verbose_level:
+            print(f"\n[{level.upper()}]: {message}")
