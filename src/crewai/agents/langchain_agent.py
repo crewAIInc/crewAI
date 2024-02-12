598c3d6640aada1a9e5a6b72a9f40d1bbb179e6b
Egor Kraev <egor.kraev@gmail.com>
2024-02-12 13:33:56 +0100

    Rename the agent for clarity

--- src/crewai/agents/langchain_agent.py
+++ src/crewai/agents/langchain_agent.py
@@ -5,7 +5,7 @@ from langchain_core.messages import AIMessage
 from crewai.agents.agent_interface import AgentWrapperParent
 
 
-class LangchainAgent(AgentWrapperParent):
+class LangchainCrewAgent(AgentWrapperParent):
 
     def __init__(
         self,
