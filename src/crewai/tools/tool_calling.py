6b51c28677c4fe646842c4fb8def31ead7dd696b
Michele Capichioni <capimichi@gmail.com>
2024-02-29 19:45:39 +0100

    Refactor ToolCalling and InstructorToolCalling classes

--- src/crewai/tools/tool_calling.py
+++ src/crewai/tools/tool_calling.py
@@ -6,16 +6,28 @@ from pydantic.v1 import BaseModel, Field
 
 
 class ToolCalling(BaseModel):
-    tool_name: str = Field(..., description="The name of the tool to be called.")
+    """
+    This class represents a tool call. It contains the name of the tool to be called and a dictionary of arguments to be passed to the tool.
+    """
+    tool_name: str = Field(
+        ..., 
+        description="The name of the tool that is intended to be called. This should be a string representing the unique identifier of the tool."
+    )
     arguments: Optional[Dict[str, Any]] = Field(
-        ..., description="A dictinary of arguments to be passed to the tool."
+        ..., 
+        description="A dictionary of arguments that are to be passed to the tool. Each key-value pair represents an argument name and its corresponding value."
     )
 
 
 class InstructorToolCalling(PydanticBaseModel):
+    """
+    This class represents a tool call made by an instructor. It contains the name of the tool to be called and a dictionary of arguments to be passed to the tool.
+    """
     tool_name: str = PydanticField(
-        ..., description="The name of the tool to be called."
+        ..., 
+        description="The name of the tool that the instructor intends to call. This should be a string representing the unique identifier of the tool."
     )
     arguments: Optional[Dict[str, Any]] = PydanticField(
-        ..., description="A dictinary of arguments to be passed to the tool."
+        ..., 
+        description="A dictionary of arguments that the instructor wants to pass to the tool. Each key-value pair represents an argument name and its corresponding value."
     )
