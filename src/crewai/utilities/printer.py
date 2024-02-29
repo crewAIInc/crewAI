9368bb51774c596031e40dd3a5ec2f3e13ac58ca
Michele Capichioni <capimichi@gmail.com>
2024-02-29 19:46:09 +0100

    Add color printing functionality to Printer class

--- src/crewai/utilities/printer.py
+++ src/crewai/utilities/printer.py
@@ -1,5 +1,13 @@
 class Printer:
+    """This class is responsible for printing colored text to the console."""
+
     def print(self, content: str, color: str):
+        """Prints the content in the specified color.
+
+        If the color is 'yellow', it calls the method to print in yellow.
+        If the color is 'red', it calls the method to print in red.
+        If any other color is specified, it prints the content without any color.
+        """
         if color == "yellow":
             self._print_yellow(content)
         elif color == "red":
@@ -8,7 +16,15 @@ class Printer:
             print(content)
 
     def _print_yellow(self, content):
+        """Prints the content in yellow.
+
+        This is done by wrapping the content in the appropriate ANSI escape codes.
+        """
         print("\033[93m {}\033[00m".format(content))
 
     def _print_red(self, content):
+        """Prints the content in red.
+
+        This is done by wrapping the content in the appropriate ANSI escape codes.
+        """
         print("\033[91m {}\033[00m".format(content))
