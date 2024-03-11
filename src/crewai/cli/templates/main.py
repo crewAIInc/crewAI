eaac6276001906704d5c0f0dcb3ba67ec90845c2
João Moura <joaomdmoura@gmail.com>
2024-03-11 19:53:34 -0300

    updating CLI template and guaranteeing tasks order

--- src/crewai/cli/templates/main.py
+++ src/crewai/cli/templates/main.py
@@ -4,5 +4,7 @@ from .crew import {{crew_name}}Crew
 
 def run():
     # Replace with your inputs, it will automatically interpolate any tasks and agents information
-    inputs = {'topic': 'AI LLMs'}
+    inputs = {
+        'topic': 'AI LLMs'
+    }
     {{crew_name}}Crew().crew().kickoff(inputs=inputs)
