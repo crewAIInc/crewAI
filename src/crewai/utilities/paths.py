6532370c30826a90c532860d8ed8f368d7c39230
João Moura <joaomdmoura@gmail.com>
2024-04-03 05:22:20 -0300

    creating db file based on package name

--- src/crewai/utilities/paths.py
+++ src/crewai/utilities/paths.py
@@ -4,9 +4,13 @@ import appdirs
 
 
 def db_storage_path():
-    app_name = "crewai"
+    app_name = get_current_package_name()
     app_author = "CrewAI"
 
     data_dir = Path(appdirs.user_data_dir(app_name, app_author))
     data_dir.mkdir(parents=True, exist_ok=True)
     return data_dir
+
+
+def get_current_package_name():
+    return __package__
