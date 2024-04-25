39aba4cb48b0fdbfc2dff63ee01ec0d6c6c22078
Mish Ushakov <10400064+mishushakov@users.noreply.github.com>
2024-04-25 12:14:56 +0000

    updated browserbase load tool

--- src/crewai_tools/tools/browserbase_load_tool/browserbase_load_tool.py
+++ src/crewai_tools/tools/browserbase_load_tool/browserbase_load_tool.py
@@ -1,12 +1,15 @@
-import os
 from crewai_tools import BaseTool
-from typing import Union, Optional
+from typing import Optional, Any
 
 class BrowserbaseLoadTool(BaseTool):
     name: str = "Browserbase web load tool"
     description: str = "Load webpages in a headless browser using Browserbase and return the contents"
+    api_key: Optional[str] = None
+    text_content: Optional[bool] = False
+    browserbase: Optional[Any] = None
 
-    def __init__(self, api_key: Optional[str] = None, text_content: bool = False):
+    def __init__(self, api_key: Optional[str] = None, text_content: Optional[bool] = False, **kwargs):
+        super().__init__(**kwargs)
         try:
             from browserbase import Browserbase
         except ImportError:
