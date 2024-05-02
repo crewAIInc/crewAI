672b1509720839de29b26fad40b38fd897f08d35
João Moura <joaomdmoura@gmail.com>
2024-05-02 04:25:11 -0300

    adding initial support for external prompt file

--- tests/utilities/test_i18n.py
+++ tests/utilities/test_i18n.py
@@ -3,32 +3,39 @@ import pytest
 from crewai.utilities.i18n import I18N
 
 
-def test_load_translation():
-    i18n = I18N(language="en")
-    i18n.load_translation()
+def test_load_prompts():
+    i18n = I18N()
+    i18n.load_prompts()
     assert i18n._translations is not None
 
 
 def test_slice():
-    i18n = I18N(language="en")
-    i18n.load_translation()
+    i18n = I18N()
+    i18n.load_prompts()
     assert isinstance(i18n.slice("role_playing"), str)
 
 
 def test_tools():
-    i18n = I18N(language="en")
-    i18n.load_translation()
+    i18n = I18N()
+    i18n.load_prompts()
     assert isinstance(i18n.tools("ask_question"), str)
 
 
 def test_retrieve():
-    i18n = I18N(language="en")
-    i18n.load_translation()
+    i18n = I18N()
+    i18n.load_prompts()
     assert isinstance(i18n.retrieve("slices", "role_playing"), str)
 
 
 def test_retrieve_not_found():
-    i18n = I18N(language="en")
-    i18n.load_translation()
+    i18n = I18N()
+    i18n.load_prompts()
     with pytest.raises(Exception):
         i18n.retrieve("nonexistent_kind", "nonexistent_key")
+
+
+def test_prompt_file():
+    i18n = I18N(prompt_file="tests/utilities/en.json")
+    i18n.load_prompts()
+    assert isinstance(i18n.retrieve("slices", "role_playing"), str)
+    assert i18n.retrieve("slices", "role_playing") == "Lorem ipsum dolor sit amet"
