ffd5942b31b253d58f987d105192e42b419123cc
João Moura <joaomdmoura@gmail.com>
2024-05-02 02:48:21 -0300

    pushing initial exa search tool

---
+++ src/crewai_tools/tools/exa_tools/exa_url_search_tool.py
@@ -0,0 +1,24 @@
+import requests
+from typing import Any
+
+from .exa_base_tool import EXABaseTool
+
+class EXAURLTool(EXABaseTool):
+	def _run(
+		self,
+		**kwargs: Any,
+	) -> Any:
+		search_query = kwargs.get('search_query')
+		if search_query is None:
+			search_query = kwargs.get('query')
+
+		payload = {
+				"query": search_query,
+		}
+
+		response = requests.post(self.search_url, json=payload, headers=self.headers)
+		results = response.json()
+		if 'results' in results:
+			results = super()._parse_results(results['results'])
+		else:
+			return results
