f175ac32d04bdb6eadfd9d091433bffb1af3cc32
João Moura <joaomdmoura@gmail.com>
2024-05-13 02:29:04 -0300

    Small RC Fixes (#608)
    * mentioning ollama on the docs as embedder
    
    * lowering barrier to match tool with simialr name
    
    * Fixing agent tools to support co_worker
    
    * Adding new tests
    
    * Fixing type"
    
    * updating tests
    
    * fixing conflict

--- tests/agent_tools/agent_tools_test.py
+++ tests/agent_tools/agent_tools_test.py
@@ -29,6 +29,20 @@ def test_delegate_work():
 
 
 @pytest.mark.vcr(filter_headers=["authorization"])
+def test_delegate_work_with_wrong_co_worker_variable():
+    result = tools.delegate_work(
+        co_worker="researcher",
+        task="share your take on AI Agents",
+        context="I heard you hate them",
+    )
+
+    assert (
+        result
+        == "AI Agents are essentially computer programs that are designed to perform tasks autonomously, with the ability to adapt and learn from their environment. These tasks range from simple ones such as setting alarms, to more complex ones like diagnosing diseases or driving cars. AI agents have the potential to revolutionize many industries, making processes more efficient and accurate. \n\nHowever, like any technology, AI agents have their downsides. They can be susceptible to biases based on the data they're trained on and they can also raise privacy concerns. Moreover, the widespread adoption of AI agents could result in significant job displacement in certain industries.\n\nDespite these concerns, it's important to note that the development and use of AI agents are heavily dependent on human decisions and policies. Therefore, the key to harnessing the benefits of AI agents while mitigating the risks lies in responsible and thoughtful development and implementation.\n\nWhether one 'loves' or 'hates' AI agents often comes down to individual perspectives and experiences. But as a researcher, it is my job to provide balanced and factual information, so I hope this explanation helps you understand better what AI Agents are and the implications they have."
+    )
+
+
+@pytest.mark.vcr(filter_headers=["authorization"])
 def test_ask_question():
     result = tools.ask_question(
         coworker="researcher",
@@ -39,6 +53,19 @@ def test_ask_question():
     assert (
         result
         == "As an AI researcher, I don't have personal feelings or emotions like love or hate. However, I recognize the importance of AI Agents in today's technological landscape. They have the potential to greatly enhance our lives and make tasks more efficient. At the same time, it is crucial to consider the ethical implications and societal impacts that come with their use. My role is to provide objective research and analysis on these topics."
+    )
+
+@pytest.mark.vcr(filter_headers=["authorization"])
+def test_ask_question_with_wrong_co_worker_variable():
+    result = tools.ask_question(
+        co_worker="researcher",
+        question="do you hate AI Agents?",
+        context="I heard you LOVE them",
+    )
+
+    assert (
+        result
+        == "No, I don't hate AI agents. In fact, I find them quite fascinating. They are powerful tools that can greatly assist in various tasks, including my research. As a technology researcher, AI and AI agents are subjects of interest to me due to their potential in advancing our understanding and capabilities in various fields. My supposed love for them stems from this professional interest and the potential they hold."
     )
 
 
