77f595e6173cf2ee8fd78f3f6bfbfe56d139a337
Jason Schrader <whoabuddy@users.noreply.github.com>
2024-05-15 15:50:49 -0700

    Add version command to CLI (#348)
    * feat: add version command to cli with tools flag
    
    * test: check output of version and tools flag
    
    * fix: add version tool info to cli outputs

--- src/crewai/cli/cli.py
+++ src/crewai/cli/cli.py
@@ -1,4 +1,5 @@
 import click
+import pkg_resources
 
 from .create_crew import create_crew
 
@@ -13,6 +14,23 @@ def crewai():
 def create(project_name):
     """Create a new crew."""
     create_crew(project_name)
+
+
+@crewai.command()
+@click.option(
+    "--tools", is_flag=True, help="Show the installed version of crewai tools"
+)
+def version(tools):
+    """Show the installed version of crewai."""
+    crewai_version = pkg_resources.get_distribution("crewai").version
+    click.echo(f"crewai version: {crewai_version}")
+
+    if tools:
+        try:
+            tools_version = pkg_resources.get_distribution("crewai[tools]").version
+            click.echo(f"crewai tools version: {tools_version}")
+        except pkg_resources.DistributionNotFound:
+            click.echo("crewai tools not installed")
 
 
 if __name__ == "__main__":
