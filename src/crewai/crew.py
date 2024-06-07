45655a956aee2c7ca32920a4419d34223abcfbc3
Braelyn Boynton <bboynton97@gmail.com>
2024-06-06 17:58:34 -0700

    conditional protect agentops use

--- src/crewai/crew.py
+++ src/crewai/crew.py
@@ -253,8 +253,9 @@ class Crew(BaseModel):
         self._set_tasks_callbacks()
 
         i18n = I18N(prompt_file=self.prompt_file)
-        agentops.set_parent_key("daebe730-f54d-4af5-98df-e6946fb76d13")
-        agentops.add_tags(["crewai"])
+        if agentops:
+            agentops.set_parent_key("daebe730-f54d-4af5-98df-e6946fb76d13")
+            agentops.add_tags(["crewai"])
 
         for agent in self.agents:
             agent.i18n = i18n
