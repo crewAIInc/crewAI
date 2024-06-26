a95f5c27c68fa846139ae81ec9478a4b9f91c553
Seth Donaldson <sethd63@gmail.com>
2024-06-26 15:52:54 -0400

    Create PDFEmbedchainAdapter class and utilize it in PDFSearchTool

--- src/crewai_tools/adapters/pdf_embedchain_adapter.py
+++ src/crewai_tools/adapters/pdf_embedchain_adapter.py
@@ -1,17 +1,23 @@
-from typing import Any
+from typing import Any, Optional
 
 from embedchain import App
 
 from crewai_tools.tools.rag.rag_tool import Adapter
 
 
-class EmbedchainAdapter(Adapter):
+class PDFEmbedchainAdapter(Adapter):
     embedchain_app: App
     summarize: bool = False
+    src: Optional[str] = None
 
     def query(self, question: str) -> str:
+        where = (
+            {"app_id": self.embedchain_app.config.id, "source": self.src}
+            if self.src
+            else None
+        )
         result, sources = self.embedchain_app.query(
-            question, citations=True, dry_run=(not self.summarize)
+            question, citations=True, dry_run=(not self.summarize), where=where
         )
         if self.summarize:
             return result
@@ -22,4 +28,5 @@ class EmbedchainAdapter(Adapter):
         *args: Any,
         **kwargs: Any,
     ) -> None:
+        self.src = args[0] if args else None
         self.embedchain_app.add(*args, **kwargs)
