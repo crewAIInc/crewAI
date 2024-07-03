430f0e9013927915475f5b07bd14317fe0e6c815
João Moura <joaomdmoura@gmail.com>
2024-07-03 18:40:19 -0400

    TYPO

--- src/crewai/agents/agent_builder/utilities/base_output_converter_base.py
+++ src/crewai/agents/agent_builder/utilities/base_output_converter_base.py
@@ -27,7 +27,7 @@ class OutputConverter(BaseModel, ABC):
     llm: Any = Field(description="The language model to be used to convert the text.")
     model: Any = Field(description="The model to be used to convert the text.")
     instructions: str = Field(description="Conversion instructions to the LLM.")
-    max_attemps: Optional[int] = Field(
+    max_attempts: Optional[int] = Field(
         description="Max number of attemps to try to get the output formated.",
         default=3,
     )
