a02d2fb93e5668857e12570d85cd06e5be272109
Brandon Hancock <brandon@brandonhancock.io>
2024-07-15 13:40:51 -0400

    Add return statement to recursive call

--- src/crewai/agent.py
+++ src/crewai/agent.py
@@ -181,7 +181,6 @@ class Agent(BaseAgent):
         self.agent_executor.tools = parsed_tools
         self.agent_executor.task = task
 
-        # TODO: COMPARE WITH ARGS AND WITHOUT ARGS
         self.agent_executor.tools_description = self._render_text_description_and_args(
             parsed_tools
         )
@@ -204,7 +203,7 @@ class Agent(BaseAgent):
             self._times_executed += 1
             if self._times_executed > self.max_retry_limit:
                 raise e
-            self.execute_task(task, context, tools)
+            return self.execute_task(task, context, tools)
 
         if self.max_rpm:
             self._rpm_controller.stop_rpm_counter()
