e8b185e607c361a84c6a3d097c3546e3a608fa00
Thiago Moretto <thiago.moretto@gmail.com>
2024-08-14 14:05:44 -0300

    Fix NL2SQL pydantic version conflict

--- src/crewai_tools/tools/nl2sql/nl2sql_tool.py
+++ src/crewai_tools/tools/nl2sql/nl2sql_tool.py
@@ -1,12 +1,11 @@
 from typing import Any, Union
 
 from ..base_tool import BaseTool
-from pydantic import Field
+from pydantic import BaseModel, Field
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
 
-from typing import Optional, Type, Any
-from pydantic.v1 import BaseModel
+from typing import Type, Any
 
 class NL2SQLToolInput(BaseModel):
     sql_query: str = Field(
