2c74efc8f2e149608397da0c517dd00388742166
Vini Brasil <vini@hey.com>
2024-10-01 18:44:08 -0300

    Change Tool Repository authentication scope (#1378)
    This commit adds a new command for adding custom PyPI indexes
    credentials to the project. This was changed because credentials are now
    user-scoped instead of organization.

--- src/crewai/cli/cli.py
+++ src/crewai/cli/cli.py
@@ -264,6 +264,7 @@ def deploy_remove(uuid: Optional[str]):
 @click.argument("handle")
 def tool_install(handle: str):
     tool_cmd = ToolCommand()
+    tool_cmd.login()
     tool_cmd.install(handle)
 
 
@@ -272,6 +273,7 @@ def tool_install(handle: str):
 @click.option("--private", "is_public", flag_value=False)
 def tool_publish(is_public: bool):
     tool_cmd = ToolCommand()
+    tool_cmd.login()
     tool_cmd.publish(is_public)
 
 
