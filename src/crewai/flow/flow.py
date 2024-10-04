4361f4ff75b7e5f5ea915a6d975ed28a0752d988
Brandon Hancock (bhancock_ai) <109994880+bhancockio@users.noreply.github.com>
2024-10-04 12:22:46 -0400

    Brandon/cre 291 flow improvements (#1390)
    * Implement joao feedback
    
    * update colors for crew nodes
    
    * clean up
    
    * more linting clean up
    
    * round legend corners
    
    ---------
    
    Co-authored-by: João Moura <joaomdmoura@gmail.com>

--- src/crewai/flow/flow.py
+++ src/crewai/flow/flow.py
@@ -7,6 +7,7 @@ from typing import Any, Callable, Dict, Generic, List, Set, Type, TypeVar, Union
 from pydantic import BaseModel
 
 from crewai.flow.flow_visualizer import plot_flow
+from crewai.flow.utils import get_possible_return_constants
 from crewai.telemetry import Telemetry
 
 T = TypeVar("T", bound=Union[BaseModel, Dict[str, Any]])
@@ -62,12 +63,10 @@ def listen(condition):
     return decorator
 
 
-def router(method, paths=None):
+def router(method):
     def decorator(func):
         func.__is_router__ = True
         func.__router_for__ = method.__name__
-        if paths:
-            func.__router_paths__ = paths
         return func
 
     return decorator
@@ -123,10 +122,11 @@ class FlowMeta(type):
                 listeners[attr_name] = (condition_type, methods)
             elif hasattr(attr_value, "__is_router__"):
                 routers[attr_value.__router_for__] = attr_name
-                if hasattr(attr_value, "__router_paths__"):
-                    router_paths[attr_name] = attr_value.__router_paths__
+                possible_returns = get_possible_return_constants(attr_value)
+                if possible_returns:
+                    router_paths[attr_name] = possible_returns
 
-                # **Register router as a listener to its triggering method**
+                # Register router as a listener to its triggering method
                 trigger_method_name = attr_value.__router_for__
                 methods = [trigger_method_name]
                 condition_type = "OR"
--- src/crewai/flow/legend_generator.py
+++ src/crewai/flow/legend_generator.py
@@ -3,6 +3,12 @@ def get_legend_items(colors):
         {"label": "Start Method", "color": colors["start"]},
         {"label": "Method", "color": colors["method"]},
         {
+            "label": "Crew Method",
+            "color": colors["bg"],
+            "border": colors["start"],
+            "dashed": False,
+        },
+        {
             "label": "Router",
             "color": colors["router"],
             "border": colors["router_border"],
@@ -22,9 +28,10 @@ def generate_legend_items_html(legend_items):
     legend_items_html = ""
     for item in legend_items:
         if "border" in item:
+            style = "dashed" if item["dashed"] else "solid"
             legend_items_html += f"""
             <div class="legend-item">
-            <div class="legend-color-box" style="background-color: {item['color']}; border: 2px dashed {item['border']};"></div>
+            <div class="legend-color-box" style="background-color: {item['color']}; border: 2px {style} {item['border']}; border-radius: 5px;"></div>
             <div>{item['label']}</div>
             </div>
             """
@@ -32,14 +39,14 @@ def generate_legend_items_html(legend_items):
             style = "dashed" if item["dashed"] else "solid"
             legend_items_html += f"""
             <div class="legend-item">
-            <div class="legend-{style}" style="border-bottom: 2px {style} {item['color']};"></div>
+            <div class="legend-{style}" style="border-bottom: 2px {style} {item['color']}; border-radius: 5px;"></div>
             <div>{item['label']}</div>
             </div>
             """
         else:
             legend_items_html += f"""
             <div class="legend-item">
-            <div class="legend-color-box" style="background-color: {item['color']};"></div>
+            <div class="legend-color-box" style="background-color: {item['color']}; border-radius: 5px;"></div>
             <div>{item['label']}</div>
             </div>
             """
