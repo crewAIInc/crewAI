462f84c1eeed0306409e91dd6e27875f3fa562d2
Eduardo Chiarotti <dudumelgaco@hotmail.com>
2024-10-11 17:48:21 -0300

    feat: add validation for poetry migration

--- src/crewai/cli/run_crew.py
+++ src/crewai/cli/run_crew.py
@@ -8,20 +8,17 @@ def run_crew() -> None:
     Run the crew by running a command in the UV environment.
     """
     command = ["uv", "run", "run_crew"]
-
     try:
-        result = subprocess.run(command, capture_output=False, text=True, check=True)
-
-        if result.stderr:
-            click.echo(result.stderr, err=True)
+        subprocess.run(command, capture_output=True, text=True, check=True)
 
     except subprocess.CalledProcessError as e:
         click.echo(f"An error occurred while running the crew: {e}", err=True)
         click.echo(e.output, err=True, nl=True)
-        click.secho(
-            "It's possible that you are using an old version of crewAI that uses poetry, please run `crewai update` to update your pyproject.toml to use uv.",
-            fg="yellow",
-        )
+        if "table found" in e.stderr:
+            click.secho(
+                "It's possible that you are using an old version of crewAI that uses poetry, please run `crewai update` to update your pyproject.toml to use uv.",
+                fg="yellow",
+            )
 
     except Exception as e:
         click.echo(f"An unexpected error occurred: {e}", err=True)
