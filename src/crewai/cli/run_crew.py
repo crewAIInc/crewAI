9714404fc903e4f6b8e759e599c28833c4a6e0b0
Eduardo Chiarotti <dudumelgaco@hotmail.com>
2024-10-11 18:58:49 -0300

    fix: warning

--- src/crewai/cli/run_crew.py
+++ src/crewai/cli/run_crew.py
@@ -14,6 +14,7 @@ def run_crew() -> None:
     except subprocess.CalledProcessError as e:
         click.echo(f"An error occurred while running the crew: {e}", err=True)
         click.echo(e.output, err=True, nl=True)
+        click.echo(e.stderr, err=True, nl=True)
         if "table found" in e.stderr:
             click.secho(
                 "It's possible that you are using an old version of crewAI that uses poetry, please run `crewai update` to update your pyproject.toml to use uv.",
