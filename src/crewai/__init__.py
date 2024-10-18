e5a18a6ccf69e3a98d89bad99f94522504da5670
João Moura <joaomdmoura@gmail.com>
2024-10-18 17:56:28 -0300

    cutting new version

--- src/crewai/__init__.py
+++ src/crewai/__init__.py
@@ -14,5 +14,5 @@ warnings.filterwarnings(
     category=UserWarning,
     module="pydantic.main",
 )
-__version__ = "0.74.0"
+__version__ = "0.74.1"
 __all__ = ["Agent", "Crew", "Process", "Task", "Pipeline", "Router", "LLM", "Flow"]
