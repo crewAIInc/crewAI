056a8e734b3527503ff1082f587a0907bd73b369
Brandon Hancock <brandon@brandonhancock.io>
2024-10-21 17:09:59 -0400

    make flow kickoff sync

--- src/crewai/cli/templates/flow/main.py
+++ src/crewai/cli/templates/flow/main.py
@@ -1,40 +1,47 @@
 #!/usr/bin/env python
-import asyncio
 from random import randint
 
 from pydantic import BaseModel
+
 from crewai.flow.flow import Flow, listen, start
+
 from .crews.poem_crew.poem_crew import PoemCrew
+
 
 class PoemState(BaseModel):
     sentence_count: int = 1
     poem: str = ""
+
 
 class PoemFlow(Flow[PoemState]):
 
     @start()
     def generate_sentence_count(self):
         print("Generating sentence count")
-        # Generate a number between 1 and 5
-        self.state.sentence_count = randint(1, 5)  
+        self.state.sentence_count = randint(1, 5)
 
     @listen(generate_sentence_count)
     def generate_poem(self):
         print("Generating poem")
-        result = PoemCrew().crew().kickoff(inputs={"sentence_count": self.state.sentence_count})
-        
+        result = (
+            PoemCrew()
+            .crew()
+            .kickoff(inputs={"sentence_count": self.state.sentence_count})
+        )
+
         print("Poem generated", result.raw)
         self.state.poem = result.raw
-        
+
     @listen(generate_poem)
     def save_poem(self):
         print("Saving poem")
         with open("poem.txt", "w") as f:
             f.write(self.state.poem)
 
-async def kickoff():
+
+def kickoff():
     poem_flow = PoemFlow()
-    await poem_flow.kickoff()
+    poem_flow.kickoff()
 
 
 def plot():
@@ -42,9 +49,5 @@ def plot():
     poem_flow.plot()
 
 
-def main():
-    asyncio.run(kickoff())
-
-
 if __name__ == "__main__":
-    main()
+    kickoff()
