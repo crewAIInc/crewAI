1f8ee15753de7d0d17066bfcc8cb7ae13317def0
Lorenze Jay <63378463+lorenzejay@users.noreply.github.com>
2024-12-03 12:27:48 -0800

    Knowledge project directory standard (#1691)
    * Knowledge project directory standard
    
    * fixed types
    
    * comment fix
    
    * made base file knowledge source an abstract class
    
    * cleaner validator on model_post_init
    
    * fix type checker
    
    * cleaner refactor
    
    * better template

--- src/crewai/knowledge/source/csv_knowledge_source.py
+++ src/crewai/knowledge/source/csv_knowledge_source.py
@@ -10,19 +10,15 @@ class CSVKnowledgeSource(BaseFileKnowledgeSource):
 
     def load_content(self) -> Dict[Path, str]:
         """Load and preprocess CSV file content."""
-        super().load_content()  # Validate the file path
-
-        file_path = (
-            self.file_path[0] if isinstance(self.file_path, list) else self.file_path
-        )
-        file_path = Path(file_path) if isinstance(file_path, str) else file_path
-
-        with open(file_path, "r", encoding="utf-8") as csvfile:
-            reader = csv.reader(csvfile)
-            content = ""
-            for row in reader:
-                content += " ".join(row) + "\n"
-        return {file_path: content}
+        content_dict = {}
+        for file_path in self.safe_file_paths:
+            with open(file_path, "r", encoding="utf-8") as csvfile:
+                reader = csv.reader(csvfile)
+                content = ""
+                for row in reader:
+                    content += " ".join(row) + "\n"
+                content_dict[file_path] = content
+        return content_dict
 
     def add(self) -> None:
         """
