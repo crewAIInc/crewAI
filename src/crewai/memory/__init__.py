ef3b93ab93f14de4d0d893a91fe441c7997c9102
Eduardo Chiarotti <dudumelgaco@hotmail.com>
2024-12-09 09:58:15 -0300

    feat: Add ContextualMemory to __init__

--- src/crewai/memory/__init__.py
+++ src/crewai/memory/__init__.py
@@ -1,6 +1,13 @@
+from .contextual.contextual_memory import ContextualMemory
 from .entity.entity_memory import EntityMemory
 from .long_term.long_term_memory import LongTermMemory
 from .short_term.short_term_memory import ShortTermMemory
 from .user.user_memory import UserMemory
 
-__all__ = ["UserMemory", "EntityMemory", "LongTermMemory", "ShortTermMemory"]
+__all__ = [
+    "UserMemory",
+    "EntityMemory",
+    "LongTermMemory",
+    "ShortTermMemory",
+    "ContextualMemory",
+]
