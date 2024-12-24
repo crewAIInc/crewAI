64b98667a35f643f2a8bbedce1acb0bde4541b6f
Tom Mahler <tom@mahler.tech>
2024-12-24 14:13:51 +0200

    fixed code interpreter tests

--- tests/tools/test_code_interpreter_tool.py
+++ tests/tools/test_code_interpreter_tool.py
@@ -7,30 +7,30 @@ from crewai_tools.tools.code_interpreter_tool.code_interpreter_tool import (
 
 
 class TestCodeInterpreterTool(unittest.TestCase):
-    @patch("crewai_tools.tools.code_interpreter_tool.code_interpreter_tool.docker")
+    @patch("crewai_tools.tools.code_interpreter_tool.code_interpreter_tool.docker_from_env")
     def test_run_code_in_docker(self, docker_mock):
         tool = CodeInterpreterTool()
         code = "print('Hello, World!')"
-        libraries_used = "numpy,pandas"
+        libraries_used = ["numpy", "pandas"]
         expected_output = "Hello, World!\n"
 
-        docker_mock.from_env().containers.run().exec_run().exit_code = 0
-        docker_mock.from_env().containers.run().exec_run().output = (
+        docker_mock().containers.run().exec_run().exit_code = 0
+        docker_mock().containers.run().exec_run().output = (
             expected_output.encode()
         )
         result = tool.run_code_in_docker(code, libraries_used)
 
         self.assertEqual(result, expected_output)
 
-    @patch("crewai_tools.tools.code_interpreter_tool.code_interpreter_tool.docker")
+    @patch("crewai_tools.tools.code_interpreter_tool.code_interpreter_tool.docker_from_env")
     def test_run_code_in_docker_with_error(self, docker_mock):
         tool = CodeInterpreterTool()
         code = "print(1/0)"
-        libraries_used = "numpy,pandas"
+        libraries_used = ["numpy", "pandas"]
         expected_output = "Something went wrong while running the code: \nZeroDivisionError: division by zero\n"
 
-        docker_mock.from_env().containers.run().exec_run().exit_code = 1
-        docker_mock.from_env().containers.run().exec_run().output = (
+        docker_mock().containers.run().exec_run().exit_code = 1
+        docker_mock().containers.run().exec_run().output = (
             b"ZeroDivisionError: division by zero\n"
         )
         result = tool.run_code_in_docker(code, libraries_used)
