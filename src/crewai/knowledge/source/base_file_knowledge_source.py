1bc914edf4417f20eeb0af68041509d871cc628f
Erick Amorim <73451993+ericklima-ca@users.noreply.github.com>
2024-12-27 20:18:25 -0400

    fix: Change storage initialization to None for KnowledgeStorage (#1804)
    * fix: Change storage initialization to None for KnowledgeStorage
    
    * refactor: Change storage field to optional and improve error handling when saving documents
    
    ---------
    
    Co-authored-by: João Moura <joaomdmoura@gmail.com>

--- src/crewai/knowledge/source/base_file_knowledge_source.py
+++ src/crewai/knowledge/source/base_file_knowledge_source.py
@@ -22,7 +22,7 @@ class BaseFileKnowledgeSource(BaseKnowledgeSource, ABC):
         default_factory=list, description="The path to the file"
     )
     content: Dict[Path, str] = Field(init=False, default_factory=dict)
-    storage: KnowledgeStorage = Field(default_factory=KnowledgeStorage)
+    storage: Optional[KnowledgeStorage] = Field(default=None)
     safe_file_paths: List[Path] = Field(default_factory=list)
 
     @field_validator("file_path", "file_paths", mode="before")
@@ -62,7 +62,10 @@ class BaseFileKnowledgeSource(BaseKnowledgeSource, ABC):
 
     def _save_documents(self):
         """Save the documents to the storage."""
-        self.storage.save(self.chunks)
+        if self.storage:
+            self.storage.save(self.chunks)
+        else:
+            raise ValueError("No storage found to save documents.")
 
     def convert_to_path(self, path: Union[Path, str]) -> Path:
         """Convert a path to a Path object."""
