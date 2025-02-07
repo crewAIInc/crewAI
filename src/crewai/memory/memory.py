a7f5d574dc6e521dec51453d9d842cf1cee908c9
Brandon Hancock (bhancock_ai) <109994880+bhancockio@users.noreply.github.com>
2025-02-07 14:45:36 -0500

    General Clean UP (#2042)
    * clean up. fix type safety. address memory config docs
    
    * improve manager
    
    * Include fix for o1 models not supporting system messages
    
    * more broad with o1
    
    * address fix: Typo in expected_output string #2045
    
    * drop prints
    
    * drop prints
    
    * wip
    
    * wip
    
    * fix failing memory tests
    
    * Fix memory provider issue
    
    * clean up short term memory
    
    * revert ltm
    
    * drop

--- src/crewai/memory/memory.py
+++ src/crewai/memory/memory.py
@@ -1,15 +1,19 @@
-from typing import Any, Dict, List, Optional
+from typing import Any, Dict, List, Optional, Union
+
+from pydantic import BaseModel
 
 from crewai.memory.storage.rag_storage import RAGStorage
 
 
-class Memory:
+class Memory(BaseModel):
     """
     Base class for memory, now supporting agent tags and generic metadata.
     """
 
-    def __init__(self, storage: RAGStorage):
-        self.storage = storage
+    storage: Any
+
+    def __init__(self, storage: Any, **data: Any):
+        super().__init__(storage=storage, **data)
 
     def save(
         self,
