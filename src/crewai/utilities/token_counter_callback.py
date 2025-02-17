8f16473f0c33bdff4f3f047ed210469e6a130a0b
pantong <ipantong@126.com>
2025-02-17 21:50:08 +0800

    fix token calculate error,if cached_tokens is None

--- src/crewai/utilities/token_counter_callback.py
+++ src/crewai/utilities/token_counter_callback.py
@@ -31,7 +31,7 @@ class TokenCalcHandler(CustomLogger):
                         self.token_cost_process.sum_prompt_tokens(usage.prompt_tokens)
                     if hasattr(usage, "completion_tokens"):
                         self.token_cost_process.sum_completion_tokens(usage.completion_tokens)
-                    if hasattr(usage, "prompt_tokens_details") and usage.prompt_tokens_details:
+                    if hasattr(usage, "prompt_tokens_details") and usage.prompt_tokens_details and usage.prompt_tokens_details.cached_tokens:
                         self.token_cost_process.sum_cached_prompt_tokens(
                             usage.prompt_tokens_details.cached_tokens
                         )
