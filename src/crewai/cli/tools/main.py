db3c8a49bd42342fbc6f5a569248bb51ff42364d
Lucas Gomide <lucaslg200@gmail.com>
2025-06-09 13:21:12 -0300

    feat: improve docs and logging for Multi-Org actions in CLI (#2980)
    * docs: add organization management in our CLI docs
    
    * feat: improve user feedback when user is not authenticated
    
    * feat: improve logging about current organization while publishing/install a Tool
    
    * feat: improve logging when Agent repository is not found during fetch
    
    * fix linter offences
    
    * test: fix auth token error

--- src/crewai/cli/tools/main.py
+++ src/crewai/cli/tools/main.py
@@ -91,6 +91,7 @@ class ToolCommand(BaseCommand, PlusAPIMixin):
             console.print(
                 f"[green]Found these tools to publish: {', '.join([e['name'] for e in available_exports])}[/green]"
             )
+        self._print_current_organization()
 
         with tempfile.TemporaryDirectory() as temp_build_dir:
             subprocess.run(
@@ -136,6 +137,7 @@ class ToolCommand(BaseCommand, PlusAPIMixin):
         )
 
     def install(self, handle: str):
+        self._print_current_organization()
         get_response = self.plus_api_client.get_tool(handle)
 
         if get_response.status_code == 404:
@@ -182,7 +184,7 @@ class ToolCommand(BaseCommand, PlusAPIMixin):
         settings.dump()
 
         console.print(
-            "Successfully authenticated to the tool repository.", style="bold green"
+            f"Successfully authenticated to the tool repository as {settings.org_name} ({settings.org_uuid}).", style="bold green"
         )
 
     def _add_package(self, tool_details: dict[str, Any]):
@@ -240,3 +242,10 @@ class ToolCommand(BaseCommand, PlusAPIMixin):
         )
 
         return env
+
+    def _print_current_organization(self):
+        settings = Settings()
+        if settings.org_uuid:
+            console.print(f"Current organization: {settings.org_name} ({settings.org_uuid})", style="bold blue")
+        else:
+            console.print("No organization currently set. We recommend setting one before using: `crewai org switch <org_id>` command.", style="yellow")
