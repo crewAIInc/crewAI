2c38d1d4487c0254fe066b983396d84dc294119e
Piyush Jain <piyushjain@duck.com>
2025-07-18 14:33:39 -0700

    Bedrock AgentCore browser and code interpreter toolkits (#385)
    * Added browser and code tools
    
    * Added dependencies, moved imports inside class
    
    * Added instructions in README
    
    * Updated imports
    
    * Updated imports
    
    * Updated dependencies
    
    * Fix 'get_current_page' utilities for Browser tools
    
    * Support browser session cleanup from synchronous code
    
    * Update browser tool examples for new changes
    
    * Manually override _run->_arun and set nested loop when in crew event loop
    
    * Browser async example
    
    * update examples with uv
    
    * Fix toolkit fields for code interpreter
    
    * Update code interpreter examples
    
    * update uv.lock
    
    * Move nest_asyncio import
    
    ---------
    
    Co-authored-by: Michael Chin <mchin188@yahoo.com>

--- src/crewai_tools/aws/__init__.py
+++ src/crewai_tools/aws/__init__.py
@@ -1,9 +1,16 @@
 from .s3 import S3ReaderTool, S3WriterTool
-from .bedrock import BedrockKBRetrieverTool, BedrockInvokeAgentTool
+from .bedrock import (
+    BedrockKBRetrieverTool,
+    BedrockInvokeAgentTool,
+    create_browser_toolkit,
+    create_code_interpreter_toolkit,
+)
 
 __all__ = [
-    'S3ReaderTool', 
-    'S3WriterTool',
-    'BedrockKBRetrieverTool',
-    'BedrockInvokeAgentTool'
+    "S3ReaderTool",
+    "S3WriterTool",
+    "BedrockKBRetrieverTool",
+    "BedrockInvokeAgentTool",
+    "create_browser_toolkit",
+    "create_code_interpreter_toolkit"
 ]
--- src/crewai_tools/aws/bedrock/__init__.py
+++ src/crewai_tools/aws/bedrock/__init__.py
@@ -1,4 +1,11 @@
 from .knowledge_base.retriever_tool import BedrockKBRetrieverTool
 from .agents.invoke_agent_tool import BedrockInvokeAgentTool
+from .browser import create_browser_toolkit
+from .code_interpreter import create_code_interpreter_toolkit
 
-__all__ = ["BedrockKBRetrieverTool", "BedrockInvokeAgentTool"]
+__all__ = [
+    "BedrockKBRetrieverTool", 
+    "BedrockInvokeAgentTool",
+    "create_browser_toolkit",
+    "create_code_interpreter_toolkit"
+]
