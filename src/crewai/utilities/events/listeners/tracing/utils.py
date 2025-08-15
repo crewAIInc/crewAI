796345932ade0ea86f205c15ad09bfdb6393c65f
lorenzejay <lorenzejaytech@gmail.com>
2025-08-15 10:40:57 -0700

    default true

--- src/crewai/utilities/events/listeners/tracing/utils.py
+++ src/crewai/utilities/events/listeners/tracing/utils.py
@@ -26,7 +26,7 @@ def on_first_execution_tracing_confirmation() -> bool:
         mark_first_execution_done()
         return click.confirm(
             "This is the first execution of CrewAI. Do you want to enable tracing?",
-            default=False,
+            default=True,
             show_default=True,
         )
     return False
