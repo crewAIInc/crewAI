9bc6d4f2cdf522f06836f9d7466a98d107e9c476
Greyson LaLonde <greyson.r.lalonde@gmail.com>
2025-08-22 12:36:14 -0400

    Add RAG config constants

---
+++ src/crewai/rag/config/constants.py
@@ -0,0 +1,6 @@
+"""Constants for RAG configuration."""
+
+from typing import Final
+
+DEFAULT_RAG_CONFIG_PATH: Final[str] = "crewai.rag.chromadb.config"
+DEFAULT_RAG_CONFIG_CLASS: Final[str] = "ChromaDBConfig"
