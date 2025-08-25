12361f15d6b8679ebd36c527aa15ec3f2c0cf539
Greyson LaLonde <greyson.r.lalonde@gmail.com>
2025-08-25 02:32:23 -0400

    Add DISCRIMINATOR constant for RAG config type discrimination

--- src/crewai/rag/config/constants.py
+++ src/crewai/rag/config/constants.py
@@ -2,5 +2,7 @@
 
 from typing import Final
 
+DISCRIMINATOR: Final[str] = "provider"
+
 DEFAULT_RAG_CONFIG_PATH: Final[str] = "crewai.rag.chromadb.config"
 DEFAULT_RAG_CONFIG_CLASS: Final[str] = "ChromaDBConfig"
