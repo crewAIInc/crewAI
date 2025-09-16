2121c22cf0a352226374c9938e5d8958455d3571
qizwiz <jonathan.f.hill@gmail.com>
2025-09-15 20:06:56 -0500

    Add tool_execution_verifier to utilities module exports

--- src/crewai/utilities/__init__.py
+++ src/crewai/utilities/__init__.py
@@ -1,4 +1,7 @@
 from .converter import Converter, ConverterError
+from .exceptions.context_window_exceeding_exception import (
+    LLMContextLengthExceededException,
+)
 from .file_handler import FileHandler
 from .i18n import I18N
 from .internal_instructor import InternalInstructor
@@ -7,20 +10,20 @@ from .parser import YamlParser
 from .printer import Printer
 from .prompts import Prompts
 from .rpm_controller import RPMController
-from .exceptions.context_window_exceeding_exception import (
-    LLMContextLengthExceededException,
-)
+from .tool_execution_verifier import ToolExecutionWrapper, execution_registry
 
 __all__ = [
+    "I18N",
     "Converter",
     "ConverterError",
     "FileHandler",
-    "I18N",
     "InternalInstructor",
+    "LLMContextLengthExceededException",
     "Logger",
     "Printer",
     "Prompts",
     "RPMController",
+    "ToolExecutionWrapper",
     "YamlParser",
-    "LLMContextLengthExceededException",
+    "execution_registry",
 ]
