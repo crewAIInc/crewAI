5d94eedfc392fb75b0449e51b1e83bfd309807ee
Greyson Lalonde <greyson.r.lalonde@gmail.com>
2025-10-20 13:04:15 -0400

    chore: remove crewai-enterprise tools

--- lib/crewai-tools/src/crewai_tools/tools/__init__.py
+++ lib/crewai-tools/src/crewai_tools/tools/__init__.py
@@ -32,9 +32,6 @@ from crewai_tools.tools.contextualai_rerank_tool.contextual_rerank_tool import (
 from crewai_tools.tools.couchbase_tool.couchbase_tool import (
     CouchbaseFTSVectorSearchTool,
 )
-from crewai_tools.tools.crewai_enterprise_tools.crewai_enterprise_tools import (
-    CrewaiEnterpriseTools,
-)
 from crewai_tools.tools.crewai_platform_tools.crewai_platform_tools import (
     CrewaiPlatformTools,
 )
@@ -199,7 +196,6 @@ __all__ = [
     "ContextualAIQueryTool",
     "ContextualAIRerankTool",
     "CouchbaseFTSVectorSearchTool",
-    "CrewaiEnterpriseTools",
     "CrewaiPlatformTools",
     "DOCXSearchTool",
     "DallETool",
