a928cde6ee1cade3dfa06c2098238c2aaad201c7
Greyson LaLonde <greyson.r.lalonde@gmail.com>
2025-11-24 16:51:28 -0500

    fix: rag tool embeddings config
    * fix: ensure config is not flattened, add tests
    
    * chore: refactor inits to model_validator
    
    * chore: refactor rag tool config parsing
    
    * chore: add initial docs
    
    * chore: add additional validation aliases for provider env vars
    
    * chore: add solid docs
    
    * chore: move imports to top
    
    * fix: revert circular import
    
    * fix: lazy import qdrant-client
    
    * fix: allow collection name config
    
    * chore: narrow model names for google
    
    * chore: update additional docs
    
    * chore: add backward compat on model name aliases
    
    * chore: add tests for config changes

---
+++ lib/crewai-tools/src/crewai_tools/tools/rag/__init__.py
@@ -0,0 +1,10 @@
+from crewai.rag.embeddings.types import ProviderSpec
+
+from crewai_tools.tools.rag.types import RagToolConfig, VectorDbConfig
+
+
+__all__ = [
+    "ProviderSpec",
+    "RagToolConfig",
+    "VectorDbConfig",
+]
