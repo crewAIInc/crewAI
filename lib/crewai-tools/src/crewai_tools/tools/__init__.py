a9f72c2491b52a012dc4df59b3daf97dee8e7cfe
Guy Hartstein <guyh2020@gmail.com>
2025-12-16 17:33:44 -0500

    feat: add Tavily Crawl Tool for crawling websites and extracting content from multiple pages

--- lib/crewai-tools/src/crewai_tools/tools/__init__.py
+++ lib/crewai-tools/src/crewai_tools/tools/__init__.py
@@ -163,6 +163,7 @@ from crewai_tools.tools.snowflake_search_tool import (
 )
 from crewai_tools.tools.spider_tool.spider_tool import SpiderTool
 from crewai_tools.tools.stagehand_tool.stagehand_tool import StagehandTool
+from crewai_tools.tools.tavily_crawl_tool.tavily_crawl_tool import TavilyCrawlTool
 from crewai_tools.tools.tavily_extractor_tool.tavily_extractor_tool import (
     TavilyExtractorTool,
 )
@@ -262,6 +263,7 @@ __all__ = [
     "SpiderTool",
     "StagehandTool",
     "TXTSearchTool",
+    "TavilyCrawlTool",
     "TavilyExtractorTool",
     "TavilySearchTool",
     "VisionTool",
