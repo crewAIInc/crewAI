b8b8c9a9bdc543913372931fbd9d215f92a4faa1
Greyson LaLonde <greyson@crewai.com>
2026-04-07 06:00:40 +0800

    docs: add storage providers section, export JsonProvider
    Document JsonProvider and SqliteProvider in all languages. Export
    JsonProvider from crewai.state alongside SqliteProvider.

--- lib/crewai/src/crewai/state/__init__.py
+++ lib/crewai/src/crewai/state/__init__.py
@@ -1,5 +1,11 @@
 from crewai.state.checkpoint_config import CheckpointConfig, CheckpointEventType
+from crewai.state.provider.json_provider import JsonProvider
 from crewai.state.provider.sqlite_provider import SqliteProvider
 
 
-__all__ = ["CheckpointConfig", "CheckpointEventType", "SqliteProvider"]
+__all__ = [
+    "CheckpointConfig",
+    "CheckpointEventType",
+    "JsonProvider",
+    "SqliteProvider",
+]
