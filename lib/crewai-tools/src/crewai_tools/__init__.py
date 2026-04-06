b8dfefcc5d8003aae94bdd5c80b51f098abcb10b
Joao Moura <joaomdmoura@gmail.com>
2026-04-06 00:33:20 -0700

    feat: bump versions to 1.14.0a2

--- lib/crewai-tools/src/crewai_tools/__init__.py
+++ lib/crewai-tools/src/crewai_tools/__init__.py
@@ -309,4 +309,4 @@ __all__ = [
     "ZapierActionTools",
 ]
 
-__version__ = "1.14.0a1"
+__version__ = "1.14.0a2"
--- lib/crewai/src/crewai/__init__.py
+++ lib/crewai/src/crewai/__init__.py
@@ -46,7 +46,7 @@ def _suppress_pydantic_deprecation_warnings() -> None:
 
 _suppress_pydantic_deprecation_warnings()
 
-__version__ = "1.14.0a1"
+__version__ = "1.14.0a2"
 _telemetry_submitted = False
 
 
