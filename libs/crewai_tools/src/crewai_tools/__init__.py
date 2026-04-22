1ed9c9865492ae439bf5b8c00ee142c168e2bcea
2Captcha <alex@2captcha.com>
2026-04-21 19:24:35 -0700

    feat: complete TwoCaptchaSolverTool implementation with proper exports

--- libs/crewai_tools/src/crewai_tools/__init__.py
+++ libs/crewai_tools/src/crewai_tools/__init__.py
@@ -1,8 +1,3 @@
+﻿from .tools.twocaptcha_solver.twocaptcha_solver import TwoCaptchaSolverTool
 
-from .tools.twocaptcha_solver.twocaptcha_solver import TwoCaptchaSolverTool
-
-if "__all__" not in globals():
-    __all__ = []
-
-if "TwoCaptchaSolverTool" not in __all__:
-    __all__.append("TwoCaptchaSolverTool")
+__all__ = ["TwoCaptchaSolverTool"]
