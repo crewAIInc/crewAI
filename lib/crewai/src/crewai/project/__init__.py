a6cf52ec7ee87237c67558b6cccf2baf970966e7
Vinicius Brasil <vini@hey.com>
2026-06-16 11:51:22 -0700

    Add inline crew definition loading (#6183)

--- lib/crewai/src/crewai/project/__init__.py
+++ lib/crewai/src/crewai/project/__init__.py
@@ -14,12 +14,22 @@ from crewai.project.annotations import (
     tool,
 )
 from crewai.project.crew_base import CrewBase
+from crewai.project.crew_definition import (
+    CrewAgentDefinition,
+    CrewDefinition,
+    CrewTaskDefinition,
+    PythonReferenceDefinition,
+)
 from crewai.project.crew_loader import load_crew, load_crew_and_kickoff
 from crewai.project.json_loader import load_agent, strip_jsonc_comments
 
 
 __all__ = [
+    "CrewAgentDefinition",
     "CrewBase",
+    "CrewDefinition",
+    "CrewTaskDefinition",
+    "PythonReferenceDefinition",
     "after_kickoff",
     "agent",
     "before_kickoff",
