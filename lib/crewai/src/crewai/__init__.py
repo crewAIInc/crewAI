38055cd65b503b7c4b38c2166763cbfb845f00ad
lorenzejay <lorenzejaytech@gmail.com>
2026-06-26 14:56:42 -0700

    feat: bump versions to 1.15.1a1

--- lib/crewai/src/crewai/__init__.py
+++ lib/crewai/src/crewai/__init__.py
@@ -48,7 +48,7 @@ def _suppress_pydantic_deprecation_warnings() -> None:
 
 _suppress_pydantic_deprecation_warnings()
 
-__version__ = "1.15.0"
+__version__ = "1.15.1a1"
 
 _LAZY_IMPORTS: dict[str, tuple[str, str]] = {
     "Memory": ("crewai.memory.unified_memory", "Memory"),
