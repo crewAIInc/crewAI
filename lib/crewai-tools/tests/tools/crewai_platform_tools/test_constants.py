e1f3c4bdd4d3f065edd560b148763bcd4e008a68
Vidit Ostwal <110953813+Vidit-Ostwal@users.noreply.github.com>
2026-09-11 21:02:00 +0530

    feat: expose CrewAI Platform application catalog (#7383)
    * feat: expose platform application catalog
    
    * refactor: centralize platform application catalog
    
    * fix: preserve platform application typing
    
    * chore: remove unused platform app export

---
+++ lib/crewai-tools/tests/tools/crewai_platform_tools/test_constants.py
@@ -0,0 +1,22 @@
+from crewai_core.platform_apps import PLATFORM_APPS
+
+
+def test_platform_apps_contains_supported_application_catalog() -> None:
+    assert PLATFORM_APPS == (
+        "asana",
+        "box",
+        "clickup",
+        "github",
+        "gmail",
+        "google_calendar",
+        "google_sheets",
+        "hubspot",
+        "jira",
+        "linear",
+        "notion",
+        "salesforce",
+        "shopify",
+        "slack",
+        "stripe",
+        "zendesk",
+    )
